13fba8a14915191c0016743d1894d8843a9c5f61
aniansson <ebba@aniansson.se>
2017-01-11 15:25:10 +0100

    Create feature and step file

---
+++ features/payment.feature
@@ -0,0 +1,19 @@
+Feature: As a Buying User,
+  In order to get my food delivered,
+  I need to confirm my order by paying through a payment solution
+
+  As a selling User
+  In order to see which of my dishes on offer have been purchased
+  I need to see payment confirmation through a payment solution
+
+  Background:
+    Given the following dishes are in shopping cart
+      |    name   |
+      | meatballs |
+      |   taco    |
+
+  Scenario: Buying User pays for the dishes in her basket
+    When I click the "Pay with Card" stripe button
+    And I fill in my card details on the stripe form
+    And I submit the stripe form
+    Then my order should be registered in the system
---
+++ features/step_definitions/payment_steps.rb
@@ -0,0 +1,20 @@
+Given(/^the following dishes are in shopping cart$/) do |table|
+  # table is a Cucumber::MultilineArgument::DataTable
+  pending # Write code here that turns the phrase above into concrete actions
+end
+
+When(/^I click the "([^"]*)" stripe button$/) do |arg1|
+  pending # Write code here that turns the phrase above into concrete actions
+end
+
+When(/^I fill in my card details on the stripe form$/) do
+  pending # Write code here that turns the phrase above into concrete actions
+end
+
+When(/^I submit the stripe form$/) do
+  pending # Write code here that turns the phrase above into concrete actions
+end
+
+Then(/^my order should be registered in the system$/) do
+  pending # Write code here that turns the phrase above into concrete actions
+end
