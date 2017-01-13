7781e1a90f1ab13bfd437aba74506683e4587446
tochman <thomas@craftacademy.se>
2017-01-13 14:52:58 +0100

    removes comments in landing controller

--- app/controllers/landing_controller.rb
+++ app/controllers/landing_controller.rb
@@ -5,9 +5,6 @@ class LandingController < ApplicationController
 
   def checkout
     @total_amount = 50
-    # ShoppingCart.find_by(id: 1).shopping_cart_items.each do |shoppingcartitem|
-    #   @totalamount += shoppingcartitem.item.price
-    # end
     @total_amount = (@total_amount / 9.11).round(2)
   end
 end
