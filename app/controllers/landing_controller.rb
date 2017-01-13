21951ea93565983fcde17f5097e68224d195f7a9
tochman <thomas@craftacademy.se>
2017-01-13 10:47:13 +0100

    refactors variable name in view and landing controller

--- app/controllers/landing_controller.rb
+++ app/controllers/landing_controller.rb
@@ -4,10 +4,10 @@ class LandingController < ApplicationController
   end
 
   def checkout
-    @totalamount = 50
+    @total_amount = 50
     # ShoppingCart.find_by(id: 1).shopping_cart_items.each do |shoppingcartitem|
     #   @totalamount += shoppingcartitem.item.price
     # end
-    @totalamount = (@totalamount / 9.11).round(2)
+    @total_amount = (@total_amount / 9.11).round(2)
   end
 end
