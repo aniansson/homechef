d643f33dedf4a76425d69fb4d8ff25001639fcd2
aniansson <ebba@aniansson.se>
2017-01-26 15:47:00 +0100

    Add location on user

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,4 +3,8 @@ class User < ApplicationRecord
   # :confirmable, :lockable, :timeoutable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :trackable, :validatable
+
+  acts_as_mappable lat_column_name: :lat,
+                   lng_column_name: :lng,
+                   default_units: :kms
 end
