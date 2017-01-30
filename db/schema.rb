5d3cfc2c9390330a8080433e321ee2528e4d7e65
aniansson <ebba@aniansson.se>
2017-01-30 13:53:36 +0100

    Create feature test

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20170127074034) do
+ActiveRecord::Schema.define(version: 20170130123704) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -23,6 +23,8 @@ ActiveRecord::Schema.define(version: 20170127074034) do
     t.integer  "portions"
     t.datetime "created_at",  null: false
     t.datetime "updated_at",  null: false
+    t.float    "lng"
+    t.float    "lat"
   end
 
   create_table "order_items", force: :cascade do |t|
