d69eb6c6a49d35b4956e056de8ac67e63bd022d4
Philip <Philip.zdb@gmail.com>
2017-01-30 10:54:44 +0100

    Changes long lat attribute to float data type

--- db/migrate/20170126135435_add_lng_and_lat_to_users.rb
+++ db/migrate/20170126135435_add_lng_and_lat_to_users.rb
@@ -1,6 +1,6 @@
 class AddLngAndLatToUsers < ActiveRecord::Migration[5.0]
   def change
-    add_column :users, :lat, :integer
-    add_column :users, :lng, :integer
+    add_column :users, :lat, :float
+    add_column :users, :lng, :float
   end
 end
