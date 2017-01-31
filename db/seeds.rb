4f06d8f52763be26187e6d106c6db80209c74833
aniansson <ebba@aniansson.se>
2017-01-31 15:58:15 +0100

    Add factorygirl for user and dish

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,7 +5,5 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
-
-
 user = FactoryGirl.create(:user)
 dish = FactoryGirl.create(:dish)
