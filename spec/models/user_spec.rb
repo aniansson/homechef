52f163b9a57f8554fea0ef79b4a0f383849439a0
aniansson <ebba@aniansson.se>
2017-02-04 19:15:43 +0100

    Create migration for attributes

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -5,6 +5,7 @@ RSpec.describe User, type: :model do
       it { is_expected.to have_db_column :id }
       it { is_expected.to have_db_column :email }
       it { is_expected.to have_db_column :encrypted_password }
+      it { is_expected.to have_db_column :username }
       it { is_expected.to have_db_column :first_name }
       it { is_expected.to have_db_column :last_name }
       it { is_expected.to have_db_column :address }
