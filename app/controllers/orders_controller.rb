eac7880c3eb8087aeef39524139a4308e1c9ead5
Philip <Philip.zdb@gmail.com>
2017-02-05 23:32:25 +0100

    Removes pry

--- app/controllers/orders_controller.rb
+++ app/controllers/orders_controller.rb
@@ -2,7 +2,6 @@ class OrdersController < ApplicationController
   before_action :authenticate_user!
 
   def add_to_order
-    binding.pry
     session[:order_id] ? @order = Order.find(session[:order_id]) : create_order
     dish = Dish.find(params[:dish_id])
     @order.add(dish, dish.price)
