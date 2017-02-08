e945e222f0492d77fd94a5b79bd1ac0bb5540916
aniansson <ebba@aniansson.se>
2017-02-08 19:37:24 +0100

    Fix json error

--- app/views/api/v1/charges/show.json.jbuilder
+++ app/views/api/v1/charges/show.json.jbuilder
@@ -2,7 +2,7 @@ json.status 'success'
 json.message 'Thank you for your order <3'
 json.order_items @order.shopping_cart_items.each do |item|
   json.name item.item.name
-  json.price item.price
-  json.ready_time item.item.ready_time
+  json.price item.item.price
+  json.ready_time item.item.ready_time.to_formatted_s(:short)
 end
 json.total @order.total.to_i*100
--- spec/requests/api/v1/charges_spec.rb
+++ spec/requests/api/v1/charges_spec.rb
@@ -15,13 +15,12 @@ RSpec.describe Api::V1::ChargesController, type: :request do
     it 'should charge users card' do
       post '/api/v1/charges', params: {
         email: user.email,
-        source: 'hello',
         order_id: @order.id
       }, headers: headers
-      response = { status: 'success', message: 'Thank you for your order <3', order_items:
-        [{ name: dish.name, price: dish.price,
-        ready_time: dish.ready_time}], total: @order.total.to_i*100 }
-      expect(response_json).to eq response
+    expected_response = { 'status' => 'success', 'message' => 'Thank you for your order <3', 'order_items' =>
+        [{ 'name' => dish.name, 'price' => dish.price,
+        'ready_time' => dish.ready_time.to_formatted_s(:short)}], 'total' => @order.total.to_i*100 }
+      expect(response_json).to eq expected_response
       expect(response.status).to eq 200
     end
   end
