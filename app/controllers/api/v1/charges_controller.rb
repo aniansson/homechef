268cb5d7305307248f083fef543c3f3ad5619058
aniansson <ebba@aniansson.se>
2017-02-09 10:26:47 +0100

    Add method in api controller

--- app/controllers/api/v1/charges_controller.rb
+++ app/controllers/api/v1/charges_controller.rb
@@ -5,7 +5,48 @@ class Api::V1::ChargesController < ApiController
     @items = @order.shopping_cart_items
     @total_amount = @order.total
     @amount = @total_amount.to_i*100
-    render 'show'
+
+    customer = Stripe::Customer.create(
+        email: params[:stripeEmail],
+        source: source(params)
+    )
+
+    charge = Stripe::Charge.create(
+        customer: customer.id,
+        amount: @amount,
+        description: 'Rails Stripe customer',
+        currency: 'usd'
+    )
+
+    if charge.paid
+      @order.update(finalized: true)
+      session.delete :order_id
+      drop_portions
+      render 'show'
+    end
+
+  # rescue Stripe::CardError => e
+  #   flash[:error] = e.message
+  #
+  #
+  # end
+
   end
 
+  private
+
+  def drop_portions
+    @order.shopping_cart_items.each do |dish_item|
+      dish = Dish.find(dish_item.item.id)
+      dish.update(portions: dish.portions - dish_item.quantity)
+    end
+  end
+
+  def stripe_token(params)
+    Rails.env.test? ? generate_test_token : params[:stripeToken]
+  end
+
+  def generate_test_token
+    StripeMock.create_test_helper.generate_card_token
+  end
 end
--- spec/requests/api/v1/charges_spec.rb
+++ spec/requests/api/v1/charges_spec.rb
@@ -15,6 +15,7 @@ RSpec.describe Api::V1::ChargesController, type: :request do
     it 'should charge users card' do
       post '/api/v1/charges', params: {
         email: user.email,
+        source: StripeMock.create_test_helper.generate_card_token,
         order_id: @order.id
       }, headers: headers
     expected_response = { 'status' => 'success', 'message' => 'Thank you for your order <3', 'order_items' =>
