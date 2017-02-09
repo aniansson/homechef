0cafed0084c108cc0f273fc8f1ad0971bafe1e97
Philip <Philip.zdb@gmail.com>
2017-02-09 14:33:19 +0100

    removes method

--- app/controllers/api/v1/registrations_controller.rb
+++ app/controllers/api/v1/registrations_controller.rb
@@ -1,16 +1,2 @@
 class Api::V1::RegistrationsController < DeviseTokenAuth::RegistrationsController
-  def account_update_params
-    params.required(:user).permit(:username,
-                                  :first_name,
-                                  :last_name,
-                                  :address,
-                                  :zip_code,
-                                  :city,
-                                  :phone,
-                                  :email,
-                                  :about_me,
-                                  :password,
-                                  :password_confirmation,
-                                  :current_password)
-  end
 end
